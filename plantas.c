c0d4778122d79912dde64f2a7044359fefb3f7f9
dvalinn <tiago.andre.amorim@gmail.com>
2021-03-26 17:09:50 +0000

    todos os memory leaks fixed -- pesquisa linear

--- plantas.c
+++ plantas.c
@@ -44,30 +44,6 @@
  * @return int retorna a posição da planta no vetor se a planta existir, senão retorna -1
  */
 int colecao_pesquisa(const colecao *haystack, const char *needle, const char tipo_pesquisa) {
-    // int lower = 0;                      // limite inferior do vetor
-    // int upper = haystack->tamanho - 1;  // limite superior do vetor
-    // int current;
-    // int diff;  // resultado da comparação entre string
-
-    // while (lower <= upper) {
-    //     current = (lower + upper) / 2;
-
-    //     if (tipo_pesquisa)
-    //         diff = strcasecmp(haystack->plantas[current]->nome_cientifico, needle);
-    //     else
-    //         diff = strcasecmp(haystack->plantas[current]->ID, needle);
-
-    //     if (!diff)
-    //         return current;
-
-    //     if (diff < 0)
-    //         upper = current - 1;
-
-    //     else
-    //         lower = current + 1;
-    // }
-    // return -1;  // não encontrado
-
     if (tipo_pesquisa) {
         for (int i = 0; i < haystack->tamanho; i++)
             if (!strcasecmp(haystack->plantas[i]->nome_cientifico, needle))
@@ -362,10 +338,9 @@ colecao *colecao_importa(const char *nome_ficheiro, const char *tipo_ordem) {
             char tmp = planta_insere(importada, nova);
             if (tmp == -1)
                 printf("\n[INFO] planta %s %s na linha %d do ficheiro %s não foi criada\n", id, nome, linha, nome_ficheiro);
-            // if (!tmp)
-            //     planta_apaga(nova);
+            if (tmp)
+                planta_apaga(nova);
         }
-        planta_apaga(nova);
     }
     free(alcunhas);
     colecao_ordena(importada, importada->tipo_ordem);
